fce6aa47d7e14ac51a2b238c35d7604cb7ab3bdc
feyza gök <feyza.gok@ozu.edu.tr>
2025-08-15 17:49:30 +0300

    data yüklendi

---
+++ main.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import numpy as np
+import pandas as pd
+
+base_dir = Path(__file__).resolve().parent
+samp_path = base_dir / 'data' / 'sample_submisson.csv'
+test_path = base_dir / 'data' / 'test.csv'
+train_path = base_dir / 'data' / 'train.csv'
+
+df_train = pd.read_csv(train_path)
+print(df_train)
